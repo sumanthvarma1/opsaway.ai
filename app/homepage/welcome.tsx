d15a71ff3c42d6ecac7d543e9c0a29cd807a323c
Sumanth Varma Dandu <sumanthvarmadandu@Sumanths-MBP.fios-router.home>
2023-10-29 01:18:09 -0400

    updated next 3 containers again

--- app/homepage/welcome.tsx
+++ app/homepage/welcome.tsx
@@ -3,15 +3,15 @@ import { NextPage } from "next";
 import ImageShape from "./imageShape";
 
 const Welcome: NextPage = () => {
-    return (
-        <div className="relative rounded-2xl h-[446px] bg-darkturquoise w-full overflow-hidden flex flex-row items-start justify-start pt-[59px] pb-[121.171875px] pr-[475px] pl-[55px] box-border text-left text-[36px] text-white font-poppins bg-lightblue">   
+  return (
+    <div className="relative rounded-2xl h-[446px] bg-darkturquoise w-full overflow-hidden flex flex-row items-start justify-start pt-[59px] pb-[121.171875px] pr-[475px] pl-[55px] box-border text-left text-[36px] text-white font-poppins bg-lightblue">
       <div className="absolute my-0 mx-[!important] h-[calc(100%_-_0.36px)] w-full top-[0px] right-[0px] bottom-[100px] left-[0px] z-[0]">
         <img
           className="absolute transform -rotate-90 left-[1000px]"
           alt=""
           src="/Vector.png"
         />
-        <ImageShape/>
+        <ImageShape />
         <div className="absolute top-[59px] left-[55px] flex flex-col items-start justify-start gap-[32px]">
           <div className="relative w-[810px] h-[174px]">
             <div className="absolute top-[-1px] left-[0px] leading-[48.24px]">
@@ -24,7 +24,8 @@ const Welcome: NextPage = () => {
             <div className="absolute top-[46.83px] left-[0px] text-[22px] leading-[39px] inline-block w-[784px] h-[127px]">
               We are at the forefront of the AI revolution, where innovation
               meets intelligence. Explore the limitless possibilities of
-              Artificial Intelligence and discover how it's shaping our world.
+              Artificial Intelligence and discover how it&apos;s shaping our
+              world.
             </div>
           </div>
           <div className="rounded-[40px] bg-white overflow-hidden flex flex-row items-start justify-start pt-[15px] px-8 pb-[17px] text-center text-[16px] text-darkslategray">
@@ -42,11 +43,7 @@ const Welcome: NextPage = () => {
         </div>
       </div>
     </div>
-    )
+  );
 };
 
 export default Welcome;
-
-  
-    
-
